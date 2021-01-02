b9b68999e230713bdbc11a97c490e23313de17a9
Mang-developer <chiwon618@gmail.com>
2021-01-02 23:47:32 +0900

    Import Header component into app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import React from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <p>TOMATO-React project</p>
+      <Header />
     </div>
   );
 }
