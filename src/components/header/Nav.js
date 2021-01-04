5eb030b14c09807c2ada9716b0f7a5016a4f6af3
Mang-developer <chiwon618@gmail.com>
2021-01-04 22:54:22 +0900

    Feat: Added Link components in nav

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default Nav = () => {
   return (
     <nav>
       <button>할일 추가하기</button>
       <ul>
-        <li>할일</li>
-        <li>목표</li>
-        <li>통계</li>
-        <li>스케줄</li>
+        <li>
+          <Link to="./">할일</Link>
+        </li>
+        <li>
+          <Link to="./">목표</Link>
+        </li>
+        <li>
+          <Link to="./">통계</Link>
+        </li>
+        <li>
+          <Link to="./">스케줄</Link>
+        </li>
       </ul>
     </nav>
   );
